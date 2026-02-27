7dbab9e6c3c9268fd511a7be22241a7d477c390e
Eunjae <110014960+eunjoyourlife@users.noreply.github.com>
2026-02-28 01:07:16 +0900

    Create setup.py for race_pkg package
    Initial setup for the race_pkg with package details and entry points.

---
+++ src/race_pkg/setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+from setuptools import setup, find_packages
+
+package_name = 'race_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # 런치 파일 등록
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # 설정 파일 등록
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='F1Tenth Racing Logic Package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # 여기서 파이썬 파일과 ROS 실행 명령어를 연결합니다.
+            'static_obstacle_detector = race_pkg.perception.static_obstacle_detector:main',
+            'centerline_publisher = race_pkg.planning.centerline_publisher:main',
+            'fgm_node = race_pkg.planning.fgm_node:main',
+            'local_planner = race_pkg.planning.local_planner:main',
+        ],
+    },
+)
